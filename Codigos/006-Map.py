b0ad949d9cf9d911dea830499d664cf49691f0a1
Raphael Medeiros <medeiros.ph@gmail.com>
2023-03-15 16:45:13 -0300

    Map

---
+++ Codigos/006-Map.py
@@ -0,0 +1,16 @@
+import apache_beam as beam
+
+file_input = r"C:\Users\Medeiros\Documents\GitHub\Apache-Beam\Dados\voos_sample.csv"
+
+p1 = beam.Pipeline()
+
+voos = (
+p1
+  | "Importar Dados" >> beam.io.ReadFromText(file_input, skip_header_lines = 1)
+  | "Separar por Vírgulas" >> beam.Map(lambda record: record.split(','))
+  | "Mostrar Resultados" >> beam.Map(print)
+)
+
+p1.run()
+
+print("Sucess Finished!")
